868baa2b3f83bb780340f2169bbd1c4c9199afe2
snowy <30571310+wen521@users.noreply.github.com>
2021-04-28 20:52:51 +0800

    大
    大的

---
+++ two_car.py
@@ -0,0 +1,458 @@
+import pandas as pd
+import numpy as np
+import warnings
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+warnings.filterwarnings('ignore')
+
+import itertools
+import matplotlib.gridspec as gridspec
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+# from mlxtend.classifier import StackingClassifier
+from sklearn.model_selection import cross_val_score, train_test_split
+# from mlxtend.plotting import plot_learning_curves
+# from mlxtend.plotting import plot_decision_regions
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.svm import SVR
+from sklearn.decomposition import PCA, FastICA, FactorAnalysis, SparsePCA
+
+import lightgbm as lgb
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# 设置与一下表格大小 防止出现省略号
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth', 1000)
+
+## 通过Pandas对于数据进行读取 (pandas是一个很友好的数据读取函数库)
+train = pd.read_csv('tianchi_data/used_car_train_20200313.csv', sep=' ')
+test = pd.read_csv('tianchi_data/used_car_testB_20200421.csv', sep=' ')
+
+# print(train.info())
+# print(test.info())
+
+# print(train.describe())
+print('------------------===================-------------------')
+
+
+# 这里我包装了一个异常值处理的代码，可以随便调用。
+def outliers_proc(data, col_name, scale=3):
+    """
+    用于清洗异常值，默认用 box_plot（scale=3）进行清洗
+    :param data: 接收 pandas 数据格式
+    :param col_name: pandas 列名
+    :param scale: 尺度
+    :return:
+    """
+
+    def box_plot_outliers(data_ser, box_scale):
+        """
+        利用箱线图去除异常值
+        :param data_ser: 接收 pandas.Series 数据格式
+        :param box_scale: 箱线图尺度，
+        :return:
+        """
+        iqr = box_scale * (data_ser.quantile(0.75) - data_ser.quantile(0.25))
+        val_low = data_ser.quantile(0.25) - iqr
+        val_up = data_ser.quantile(0.75) + iqr
+        rule_low = (data_ser < val_low)
+        rule_up = (data_ser > val_up)
+        return (rule_low, rule_up), (val_low, val_up)
+
+    data_n = data.copy()
+    data_series = data_n[col_name]
+    rule, value = box_plot_outliers(data_series, box_scale=scale)
+    index = np.arange(data_series.shape[0])[rule[0] | rule[1]]
+    print("Delete number is: {}".format(len(index)))
+    data_n = data_n.drop(index)
+    data_n.reset_index(drop=True, inplace=True)
+    print("Now column number is: {}".format(data_n.shape[0]))
+    index_low = np.arange(data_series.shape[0])[rule[0]]
+    outliers = data_series.iloc[index_low]
+    print("Description of data less than the lower bound is:")
+    print(pd.Series(outliers).describe())
+    index_up = np.arange(data_series.shape[0])[rule[1]]
+    outliers = data_series.iloc[index_up]
+    print("Description of data larger than the upper bound is:")
+    print(pd.Series(outliers).describe())
+
+    fig, ax = plt.subplots(1, 2, figsize=(10, 7))
+    sns.boxplot(y=data[col_name], data=data, palette="Set1", ax=ax[0])
+    sns.boxplot(y=data_n[col_name], data=data_n, palette="Set1", ax=ax[1])
+    return data_n
+
+
+# 我们可以删掉一些异常数据，以 power 为例。
+# 这里删不删同学可以自行判断
+# 但是要注意 test 的数据不能删 = = 不能掩耳盗铃是不是
+
+Train_data = outliers_proc(train, 'power', scale=3)
+
+# 训练集和测试集放在一起，方便构造特征
+Train_data['train'] = 1
+test['train'] = 0
+data = pd.concat([Train_data, test], ignore_index=True)
+# 使用时间：data['creatDate'] - data['regDate']，反应汽车使用时间，一般来说价格与使用时间成反比
+# 不过要注意，数据里有时间出错的格式，所以我们需要 errors='coerce'
+data['used_time'] = (pd.to_datetime(data['creatDate'], format='%Y%m%d', errors='coerce') -
+                     pd.to_datetime(data['regDate'], format='%Y%m%d', errors='coerce')).dt.days
+
+# 看一下空数据，有 15k 个样本的时间是有问题的，我们可以选择删除，也可以选择放着。
+# 但是这里不建议删除，因为删除缺失数据占总样本量过大，7.5%
+# 我们可以先放着，因为如果我们 XGBoost 之类的决策树，其本身就能处理缺失值，所以可以不用管；
+data['used_time'].isnull().sum()
+
+# 从邮编中提取城市信息，相当于加入了先验知识
+data['city'] = data['regionCode'].apply(lambda x: str(x)[:-3])
+data = data
+
+# 计算某品牌的销售统计量，同学们还可以计算其他特征的统计量
+# 这里要以 train 的数据计算统计量
+Train_gb = Train_data.groupby("brand")
+all_info = {}
+for kind, kind_data in Train_gb:
+    info = {}
+    kind_data = kind_data[kind_data['price'] > 0]
+    info['brand_amount'] = len(kind_data)
+    info['brand_price_max'] = kind_data.price.max()
+    info['brand_price_median'] = kind_data.price.median()
+    info['brand_price_min'] = kind_data.price.min()
+    info['brand_price_sum'] = kind_data.price.sum()
+    info['brand_price_std'] = kind_data.price.std()
+    info['brand_price_average'] = round(kind_data.price.sum() / (len(kind_data) + 1), 2)
+    all_info[kind] = info
+brand_fe = pd.DataFrame(all_info).T.reset_index().rename(columns={"index": "brand"})
+data = data.merge(brand_fe, how='left', on='brand')
+
+# 数据分桶 以 power 为例
+# 这时候我们的缺失值也进桶了，
+# 为什么要做数据分桶呢，原因有很多，= =
+# 1. 离散后稀疏向量内积乘法运算速度更快，计算结果也方便存储，容易扩展；
+# 2. 离散后的特征对异常值更具鲁棒性，如 age>30 为 1 否则为 0，对于年龄为 200 的也不会对模型造成很大的干扰；
+# 3. LR 属于广义线性模型，表达能力有限，经过离散化后，每个变量有单独的权重，这相当于引入了非线性，能够提升模型的表达能力，加大拟合；
+# 4. 离散后特征可以进行特征交叉，提升表达能力，由 M+N 个变量编程 M*N 个变量，进一步引入非线形，提升了表达能力；
+# 5. 特征离散后模型更稳定，如用户年龄区间，不会因为用户年龄长了一岁就变化
+
+# 当然还有很多原因，LightGBM 在改进 XGBoost 时就增加了数据分桶，增强了模型的泛化性
+
+bin = [i * 10 for i in range(31)]
+data['power_bin'] = pd.cut(data['power'], bin, labels=False)
+print(data[['power_bin', 'power']].head())
+# 删除不需要的数据
+data = data.drop(['creatDate', 'regDate', 'regionCode'], axis=1)
+print(data.shape, data.columns)
+
+# 目前的数据其实已经可以给树模型使用了，所以我们导出一下
+data.to_csv('data_for_tree.csv', index=0)
+
+# 我们可以再构造一份特征给 LR NN 之类的模型用
+# 之所以分开构造是因为，不同模型对数据集的要求不同
+# 我们看下数据分布：
+data['power'].plot.hist()
+plt.show()
+
+# 我们刚刚已经对 train 进行异常值处理了，但是现在还有这么奇怪的分布是因为 test 中的 power 异常值，
+# 所以我们其实刚刚 train 中的 power 异常值不删为好，可以用长尾分布截断来代替
+Train_data['power'].plot.hist()
+plt.show()
+
+# 我们对其取 log，在做归一化
+# from sklearn import preprocessing
+# min_max_scaler = preprocessing.MinMaxScaler()
+data['power'] = np.log(data['power'] + 1)
+data['power'] = ((data['power'] - np.min(data['power'])) / (np.max(data['power']) - np.min(data['power'])))
+data['power'].plot.hist()
+plt.show()
+
+# km 的比较正常，应该是已经做过分桶了
+data['kilometer'].plot.hist()
+plt.show()
+
+# 所以我们可以直接做归一化
+data['kilometer'] = ((data['kilometer'] - np.min(data['kilometer'])) /
+                     (np.max(data['kilometer']) - np.min(data['kilometer'])))
+data['kilometer'].plot.hist()
+plt.show()
+
+
+# 除此之外 还有我们刚刚构造的统计量特征：
+# 'brand_amount', 'brand_price_average', 'brand_price_max',
+# 'brand_price_median', 'brand_price_min', 'brand_price_std',
+# 'brand_price_sum'
+# 这里不再一一举例分析了，直接做变换，
+def max_min(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+
+data['brand_amount'] = ((data['brand_amount'] - np.min(data['brand_amount'])) /
+                        (np.max(data['brand_amount']) - np.min(data['brand_amount'])))
+data['brand_price_average'] = ((data['brand_price_average'] - np.min(data['brand_price_average'])) /
+                               (np.max(data['brand_price_average']) - np.min(data['brand_price_average'])))
+data['brand_price_max'] = ((data['brand_price_max'] - np.min(data['brand_price_max'])) /
+                           (np.max(data['brand_price_max']) - np.min(data['brand_price_max'])))
+data['brand_price_median'] = ((data['brand_price_median'] - np.min(data['brand_price_median'])) /
+                              (np.max(data['brand_price_median']) - np.min(data['brand_price_median'])))
+data['brand_price_min'] = ((data['brand_price_min'] - np.min(data['brand_price_min'])) /
+                           (np.max(data['brand_price_min']) - np.min(data['brand_price_min'])))
+data['brand_price_std'] = ((data['brand_price_std'] - np.min(data['brand_price_std'])) /
+                           (np.max(data['brand_price_std']) - np.min(data['brand_price_std'])))
+data['brand_price_sum'] = ((data['brand_price_sum'] - np.min(data['brand_price_sum'])) /
+                           (np.max(data['brand_price_sum']) - np.min(data['brand_price_sum'])))
+
+# 对类别特征进行 OneEncoder
+data = pd.get_dummies(data, columns=['model', 'brand', 'bodyType', 'fuelType',
+                                     'gearbox', 'notRepairedDamage', 'power_bin'])
+print(data.shape, data.columns)
+
+# 这份数据可以给 LR 用
+data.to_csv('data_for_lr.csv', index=0)
+
+# 相关性分析
+print(data['power'].corr(data['price'], method='spearman'))
+print(data['kilometer'].corr(data['price'], method='spearman'))
+print(data['brand_amount'].corr(data['price'], method='spearman'))
+print(data['brand_price_average'].corr(data['price'], method='spearman'))
+print(data['brand_price_max'].corr(data['price'], method='spearman'))
+print(data['brand_price_median'].corr(data['price'], method='spearman'))
+print('========-----------------------=============')
+
+# 当然也可以直接看图
+data_numeric = data[['power', 'kilometer', 'brand_amount', 'brand_price_average',
+                     'brand_price_max', 'brand_price_median']]
+correlation = data_numeric.corr()
+
+f, ax = plt.subplots(figsize=(7, 7))
+plt.title('Correlation of Numeric Features with Price', y=1, size=16)
+sns.heatmap(correlation, square=True, vmax=0.8)
+plt.show()
+
+numerical_cols = Train_data.select_dtypes(exclude='object').columns
+print(numerical_cols)
+feature_cols = [col for col in numerical_cols if col not in ['SaleID', 'name', 'regDate', 'price']]
+X_data = Train_data[feature_cols]
+Y_data = Train_data['price']
+
+X_test = test[feature_cols]
+
+print('X train shape:', X_data.shape)
+print('X test shape:', X_test.shape)
+
+
+def Sta_inf(data):
+    print('_min', np.min(data))
+    print('_max:', np.max(data))
+    print('_mean', np.mean(data))
+    print('_ptp', np.ptp(data))
+    print('_std', np.std(data))
+    print('_var', np.var(data))
+
+
+print('Sta of label:')
+Sta_inf(Y_data)
+
+X_data = X_data.fillna(-1)
+X_test = X_test.fillna(-1)
+
+
+def build_model_lr(x_train, y_train):
+    reg_model = linear_model.LinearRegression()
+    reg_model.fit(x_train, y_train)
+    return reg_model
+
+
+def build_model_ridge(x_train, y_train):
+    reg_model = linear_model.Ridge(alpha=0.8)  # alphas=range(1,100,5)
+    reg_model.fit(x_train, y_train)
+    return reg_model
+
+
+def build_model_lasso(x_train, y_train):
+    reg_model = linear_model.LassoCV()
+    reg_model.fit(x_train, y_train)
+    return reg_model
+
+
+def build_model_gbdt(x_train, y_train):
+    estimator = GradientBoostingRegressor(loss='ls', subsample=0.85, max_depth=5, n_estimators=100)
+    param_grid = {
+        'learning_rate': [0.05, 0.08, 0.1, 0.2],
+    }
+    gbdt = GridSearchCV(estimator, param_grid, cv=3)
+    gbdt.fit(x_train, y_train)
+    print(gbdt.best_params_)
+    # print(gbdt.best_estimator_ )
+    return gbdt
+
+
+def build_model_xgb(x_train, y_train):
+    model = xgb.XGBRegressor(n_estimators=120, learning_rate=0.08, gamma=0, subsample=0.8, \
+                             colsample_bytree=0.9, max_depth=5)  # , objective ='reg:squarederror'
+    model.fit(x_train, y_train)
+    return model
+
+
+def build_model_lgb(x_train, y_train):
+    estimator = lgb.LGBMRegressor(num_leaves=63, n_estimators=100)
+    param_grid = {
+        'learning_rate': [0.01, 0.05, 0.1],
+    }
+    gbm = GridSearchCV(estimator, param_grid)
+    gbm.fit(x_train, y_train)
+    return gbm
+
+
+## xgb
+xgr = xgb.XGBRegressor(n_estimators=120, learning_rate=0.1, subsample=0.8, \
+                       colsample_bytree=0.9, max_depth=7)  # ,objective ='reg:squarederror'
+
+scores_train = []
+scores = []
+
+## 5折交叉验证方式
+sk = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+for train_ind, val_ind in sk.split(X_data, Y_data):
+    train_x = X_data.iloc[train_ind].values
+    train_y = Y_data.iloc[train_ind]
+    val_x = X_data.iloc[val_ind].values
+    val_y = Y_data.iloc[val_ind]
+
+    xgr.fit(train_x, train_y)
+    pred_train_xgb = xgr.predict(train_x)
+    pred_xgb = xgr.predict(val_x)
+
+    score_train = mean_absolute_error(train_y, pred_train_xgb)
+    scores_train.append(score_train)
+    score = mean_absolute_error(val_y, pred_xgb)
+    scores.append(score)
+
+print('Train mae:', np.mean(score_train))
+print('Val mae', np.mean(scores))
+
+## Split data with val
+x_train, x_val, y_train, y_val = train_test_split(X_data, Y_data, test_size=0.3)
+
+## Train and Predict
+print('Predict LR...')
+model_lr = build_model_lr(x_train, y_train)
+val_lr = model_lr.predict(x_val)
+subA_lr = model_lr.predict(X_test)
+
+print('Predict Ridge...')
+model_ridge = build_model_ridge(x_train, y_train)
+val_ridge = model_ridge.predict(x_val)
+subA_ridge = model_ridge.predict(X_test)
+
+print('Predict Lasso...')
+model_lasso = build_model_lasso(x_train, y_train)
+val_lasso = model_lasso.predict(x_val)
+subA_lasso = model_lasso.predict(X_test)
+
+print('Predict GBDT...')
+model_gbdt = build_model_gbdt(x_train, y_train)
+val_gbdt = model_gbdt.predict(x_val)
+subA_gbdt = model_gbdt.predict(X_test)
+
+print('predict XGB...')
+model_xgb = build_model_xgb(x_train, y_train)
+val_xgb = model_xgb.predict(x_val)
+subA_xgb = model_xgb.predict(X_test)
+
+print('predict lgb...')
+model_lgb = build_model_lgb(x_train, y_train)
+val_lgb = model_lgb.predict(x_val)
+subA_lgb = model_lgb.predict(X_test)
+
+print('Sta inf of lgb:')
+Sta_inf(subA_lgb)
+
+
+def Weighted_method(test_pre1, test_pre2, test_pre3, w=[1 / 3, 1 / 3, 1 / 3]):
+    Weighted_result = w[0] * pd.Series(test_pre1) + w[1] * pd.Series(test_pre2) + w[2] * pd.Series(test_pre3)
+    return Weighted_result
+
+
+## Init the Weight
+w = [0.3, 0.4, 0.3]
+
+## 测试验证集准确度
+val_pre = Weighted_method(val_lgb, val_xgb, val_gbdt, w)
+MAE_Weighted = mean_absolute_error(y_val, val_pre)
+print('MAE of Weighted of val:', MAE_Weighted)
+
+## 预测数据部分
+subA = Weighted_method(subA_lgb, subA_xgb, subA_gbdt, w)
+print('Sta inf:')
+Sta_inf(subA)
+## 生成提交文件
+sub = pd.DataFrame()
+sub['SaleID'] = X_test.index
+sub['price'] = subA
+sub.to_csv('./new_sub_Weighted.csv', index=False)
+
+## 与简单的LR（线性回归）进行对比
+val_lr_pred = model_lr.predict(x_val)
+MAE_lr = mean_absolute_error(y_val, val_lr_pred)
+print('MAE of lr:', MAE_lr)
+
+## Starking
+
+## 第一层
+train_lgb_pred = model_lgb.predict(x_train)
+train_xgb_pred = model_xgb.predict(x_train)
+train_gbdt_pred = model_gbdt.predict(x_train)
+
+Strak_X_train = pd.DataFrame()
+Strak_X_train['Method_1'] = train_lgb_pred
+Strak_X_train['Method_2'] = train_xgb_pred
+Strak_X_train['Method_3'] = train_gbdt_pred
+
+Strak_X_val = pd.DataFrame()
+Strak_X_val['Method_1'] = val_lgb
+Strak_X_val['Method_2'] = val_xgb
+Strak_X_val['Method_3'] = val_gbdt
+
+Strak_X_test = pd.DataFrame()
+Strak_X_test['Method_1'] = subA_lgb
+Strak_X_test['Method_2'] = subA_xgb
+Strak_X_test['Method_3'] = subA_gbdt
+
+print(Strak_X_test.head())
+
+## level2-method
+model_lr_Stacking = build_model_lr(Strak_X_train, y_train)
+## 训练集
+train_pre_Stacking = model_lr_Stacking.predict(Strak_X_train)
+print('MAE of Stacking-LR:', mean_absolute_error(y_train, train_pre_Stacking))
+
+## 验证集
+val_pre_Stacking = model_lr_Stacking.predict(Strak_X_val)
+print('MAE of Stacking-LR:', mean_absolute_error(y_val, val_pre_Stacking))
+
+## 预测集
+print('Predict Stacking-LR...')
+subA_Stacking = model_lr_Stacking.predict(Strak_X_test)
+
+subA_Stacking[subA_Stacking < 10] = 10  ## 去除过小的预测值
+
+sub = pd.DataFrame()
+sub['SaleID'] = test.SaleID
+sub['price'] = subA_Stacking
+sub.to_csv('./sub_Stacking.csv', index=False)
+
+print('Sta inf:')
+Sta_inf(subA_Stacking)
